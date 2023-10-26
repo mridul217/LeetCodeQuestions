4b2217f729af7f10d05ac7a6fe8531de76777a3d
Mr Mridul <51329846+mridul217@users.noreply.github.com>
2023-10-26 23:53:36 +0530

    Time: 19 ms (17.29%), Space: 20.2 MB (7.02%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cand = 0, count = 0;
+        for (int i=0; i < nums.size();i++){
+            if (count == 0)
+                cand = nums[i];
+            if (cand == nums[i])
+                count++;
+            else
+                count--;
+        }
+        return cand;
+    }
+};
